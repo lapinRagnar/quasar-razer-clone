5f2750ae297ad568d8b20dea4c4743cda6dacf16
lapinRagnar <lapinragnar@gmail.com>
2022-12-27 11:33:12 +0100

    mettre le dark mode

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    > -->
+    <div>Salut</div>
   </q-page>
 </template>
 
